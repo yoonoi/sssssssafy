8fce45e3c53a122a62366c512b239e24ebbc760d
yoonoi <dbddbd39@naver.com>
2023-01-15 21:38:49 +0900

    2751. 수 정렬하기2
    실행시간 초과해서 ㅠㅠ 구글링함

---
+++ baekjoon/src/silver/s5/No2751_0.java
@@ -0,0 +1,50 @@
+package silver.s5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class No2751_0 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// 여러가지 방법 다 복습
+		// 1. 제일 기본 Scanner 사용
+		/*
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int[] arr = new int[num];
+		
+		for(int i=0; i<num; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		for(int i : arr) {
+			System.out.println(i);
+		}
+		*/
+		
+		// 2. BufferedReader & Collection & StringBuilder 사용하기
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		for(int i=0; i<num; i++) {
+			list.add(Integer.parseInt(br.readLine()));
+		}
+		
+		Collections.sort(list);
+		for(int i : list) {
+			sb.append(i).append("\n");
+		}
+		
+		System.out.print(sb);
+		
+	}
+
+}
---
+++ baekjoon/src/silver/s5/No2751_2.java
@@ -0,0 +1,31 @@
+package silver.s5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class No2751_2 {
+	// 2. BufferedReader 사용해서 실행시간 줄이기
+	public static void main(String[] args) throws IOException {
+		long startTime = System.currentTimeMillis();
+		
+		// 수 정렬하기2
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(arr);
+		for(int i=0; i<n; i++) {
+			System.out.println(arr[i]);
+		}
+		
+		long endTime = System.currentTimeMillis();
+		System.out.println(endTime-startTime);
+	}
+
+}
