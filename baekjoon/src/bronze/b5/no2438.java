7a5c3e51ce212ff506e831f289c8f171f596c63f
Yoonoi <dbddbd39@naver.com>
2023-01-15 19:15:25 +0900

    브5 별찍기

---
+++ baekjoon/src/bronze/b5/no2438.java
@@ -0,0 +1,18 @@
+package bronze.b5;
+
+import java.util.Scanner;
+
+public class no2438 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		
+		for(int i=0; i<size; i++) {
+			for(int j=i; j>=0; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
