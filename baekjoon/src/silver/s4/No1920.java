e33ee729f9c3754d3c2921e6b7a1886461545f79
Yoonoi <dbddbd39@naver.com>
2023-01-16 15:20:07 +0900

    1920. 수찾기

---
+++ baekjoon/src/silver/s4/No1920.java
@@ -0,0 +1,30 @@
+package silver.s4;
+
+import java.util.Scanner;
+
+public class No1920 {
+	//시간초과
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int [] arr = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int m = sc.nextInt();
+		
+		for(int i=0; i<m; i++) {
+			int result = 0;
+			int num = sc.nextInt();
+			for(int j=0; j<arr.length; j++) {
+				if(num == arr[j]) {
+					result = 1;
+					break;
+				}
+			}
+			System.out.println(result);
+		}
+	}
+}
---
+++ baekjoon/src/silver/s4/No1920_2.java
@@ -0,0 +1,36 @@
+package silver.s4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class No1920_2 {
+	//시간초과
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		while(st.hasMoreTokens()) {
+			int token = Integer.parseInt(st.nextToken());
+			list.add(token);
+		}
+		
+		int m = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		while(st.hasMoreTokens()) {
+			int result = 0;
+			
+			int token = Integer.parseInt(st.nextToken());
+			if (list.contains(token)) result = 1;
+			System.out.println(result);
+		}
+	}
+}
